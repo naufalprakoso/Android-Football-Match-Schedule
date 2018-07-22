5708688d6d4c9e3cd75a144e5bfd570ff068316a
naufalprakoso <naufalprakoso24@gmail.com>
2018-07-22 14:26:21 +0700

    Initial commit

---
+++ app/src/main/java/com/fj/footballmatchscedulefinal/InitialActivity.kt
@@ -0,0 +1,12 @@
+package com.fj.footballmatchscedulefinal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InitialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+    }
+}
